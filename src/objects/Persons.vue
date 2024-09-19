75269d42362d7e4b3f3941a4c59638c490280632
ochinchind <amanrahim93@gmail.com>
2024-09-19 22:26:47 +0500

    persons

---
+++ src/objects/Persons.vue
@@ -0,0 +1,30 @@
+<script>
+export default {
+  data() {
+    return {
+      persons: [
+        { id: 1, PersonName: 'Vladimir', Avatar: 'avatar.png', PubDate: '2024-09-15', Rating: 4.3, Commentary: 'Great product!', Topic: 'IT' },
+        { id: 2, PersonName: 'Marlen', Avatar: 'avatar.png', PubDate: '2024-09-15', Rating: 4.3, Commentary: 'Great product!', Topic: 'IT' },
+        { id: 3, PersonName: 'JackJones', Avatar: 'avatar.png', PubDate: '2024-09-15', Rating: 4.3, Commentary: 'Great product!', Topic: 'IT' },
+        { id: 4, PersonName: 'Michael', Avatar: 'avatar.png', PubDate: '2024-09-15', Rating: 4.3, Commentary: 'Great product!', Topic: 'IT' },
+        { id: 5, PersonName: 'Jaseh', Avatar: 'avatar.png', PubDate: '2024-09-15', Rating: 4.3, Commentary: 'Great product!', Topic: 'IT' },
+        { id: 6, PersonName: 'Onfroy', Avatar: 'avatar.png', PubDate: '2024-09-15', Rating: 4.3, Commentary: 'Great product!', Topic: 'IT' },
+        { id: 7, PersonName: 'Billie', Avatar: 'avatar.png', PubDate: '2024-09-15', Rating: 4.3, Commentary: 'Great product!', Topic: 'IT' },
+        { id: 8, PersonName: 'Henry', Avatar: 'avatar.png', PubDate: '2024-09-15', Rating: 4.3, Commentary: 'Great product!', Topic: 'IT' },
+        { id: 9, PersonName: 'Shahboz', Avatar: 'avatar.png', PubDate: '2024-09-15', Rating: 4.3, Commentary: 'Great product!', Topic: 'IT' },
+        { id: 10, PersonName: 'Nikolai', Avatar: 'avatar.png', PubDate: '2024-09-15', Rating: 4.3, Commentary: 'Great product!', Topic: 'IT' },
+        { id: 11, PersonName: 'Tima', Avatar: 'avatar.png', PubDate: '2024-09-15', Rating: 4.3, Commentary: 'Great product!', Topic: 'IT' },
+        { id: 12, PersonName: 'Timur', Avatar: 'avatar.png', PubDate: '2024-09-15', Rating: 4.3, Commentary: 'Great product!', Topic: 'IT' },
+        { id: 13, PersonName: 'Temirlan', Avatar: 'avatar.png', PubDate: '2024-09-15', Rating: 4.3, Commentary: 'Great product!', Topic: 'IT' },
+        { id: 14, PersonName: 'Serafim', Avatar: 'avatar.png', PubDate: '2024-09-15', Rating: 4.3, Commentary: 'Great product!', Topic: 'IT' },
+        { id: 15, PersonName: 'Steven', Avatar: 'avatar.png', PubDate: '2024-09-15', Rating: 4.3, Commentary: 'Great product!', Topic: 'IT' },
+        { id: 16, PersonName: 'Maks', Avatar: 'avatar.png', PubDate: '2024-09-15', Rating: 4.3, Commentary: 'Great product!', Topic: 'IT' },
+        { id: 17, PersonName: 'Akniet', Avatar: 'avatar.png', PubDate: '2024-09-15', Rating: 4.3, Commentary: 'Great product!', Topic: 'IT' },
+        { id: 18, PersonName: 'Baur', Avatar: 'avatar.png', PubDate: '2024-09-15', Rating: 4.3, Commentary: 'Great product!', Topic: 'IT' },
+        { id: 19, PersonName: 'Ainur', Avatar: 'avatar.png', PubDate: '2024-09-15', Rating: 4.3, Commentary: 'Great product!', Topic: 'IT' },
+        { id: 20, PersonName: 'Hawkin', Avatar: 'avatar.png', PubDate: '2024-09-15', Rating: 4.3, Commentary: 'Great product!', Topic: 'IT' },
+      ]
+    };
+  }
+};
+</script>
